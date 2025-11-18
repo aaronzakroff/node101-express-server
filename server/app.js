64525cf1179836c4a4327e1d14b04be6516fc625
aaronzakroff <aaron.zakroff@cccareers.org>
2025-11-18 10:26:58 -0800

    reupload

--- server/app.js
+++ server/app.js
@@ -1,10 +1,29 @@
-// import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const data = require('./data.json');
 
+const app = express();
 
-// create your express server below
-var app;
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// add your routes and middleware below
+// Serve data
+app.get('/data', (req, res) => {
+  res.json(data);
+});
 
-// finally export the express application
+// Root route
+app.get('/', (req, res) => {
+  res.status(200).send("It's working");
+});
+
 module.exports = app;
+
+// Only start server when run directly
+if (require.main === module) {
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}
